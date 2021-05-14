63dba165b5a1480af6c3676796c9df242edee08c
Jean Colombel <jeancolombel93@gmail.com>
2021-05-14 11:42:29 -0400

    Checkpoint before heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: %i[index new create show] do
+  resources :lists, only: %i[index new create show edit update] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
 end
-
