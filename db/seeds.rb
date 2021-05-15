3e4459477d5573e4567e3e8cab54e7b57ef33d05
Jean Colombel <jeancolombel93@gmail.com>
2021-05-15 10:28:47 -0400

    Checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,23 @@
-# Cleaning the database
-puts "Creating movies..."
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
-puts "All done"
-puts "#{Movie.count} movies created."
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning the database...'
+Movie.destroy_all
+
+puts 'Fetching top rated movies...'
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+json = open(url).read
+movies = JSON.parse(json)['results']
+
+puts 'Adding movies to the database'
+movies.each_with_index do |movie, index|
+  movie_title = movie['title']
+  puts "#{index}. #{movie_title}"
+  movie_overview = movie['overview']
+  movie_poster = "https://image.tmdb.org/t/p/w500#{movie['poster_path']}"
+  Movie.create!(title: movie_title,
+                overview: movie_overview,
+                postser_url: movie_poster)
+end
+
+puts 'All done!'
